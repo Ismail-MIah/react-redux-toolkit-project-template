7d4cce97ee2557de5df90301e125b9b0ad8238bf
ismail-miah <miahismail654@gmail.com>
2022-09-16 15:54:59 +0600

    demoslice.js badcode

--- src/features/demo/demoSlice.js
+++ src/features/demo/demoSlice.js
@@ -3,9 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {};
 
 const demoSlice = createSlice({
-  name: 'demo',
-  initialState,
-  reducers: {}
+  
 });
 // export const {} = demoSlice.actions;
 export default demoSlice.reducer;
